424adb483aebbb69c4926c53855d8d999d182714
ilmnayccmm <dzhuliia.havkaliuk@ukd.edu.ua>
2025-02-21 12:21:57 +0200

    add my exclusive  lane

--- first_task.py
+++ first_task.py
@@ -1,3 +1,3 @@
 print("Hi, my name is Андріїв Владислав Ігорович and I’m studying software development in college")
 print("Hi, my name is Федоронько Олександр Миколайович and I’m studying software development in college")
-
+print("Hi, my name is Засімук Юрій Любомирович and I’m studying software development in college")
